31628374ae7845d81569cf5eb41701adfe4aa893
Amas5amaso <ansamatemeso@gmail.com>
2023-09-08 07:04:02 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: always 0
+ * main - A program that print a line wth puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 

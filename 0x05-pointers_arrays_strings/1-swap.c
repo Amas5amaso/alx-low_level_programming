7fcdf32daa40e2996757199f5cbaed68828d6345
Amas5amaso <ansamatemeso@gmail.com>
2023-09-19 05:06:29 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,22 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-* main - check the code
-* Return: Always (0)
-*/
-
-	int main(void)
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
 {
-	int a;
-	int b;
-	a = 98;
-	b = 42;
+	int m;
 
-	printf("a=%d, b=%d\n",'a', 'b');
-	Swap_int (&a, &b);
-	printf("a=%d, b=%d\n" ,'a', 'b');
-	return (0);
+	m = *a;
+	*a = *b;
+	*b = m;
 }
+
 
 

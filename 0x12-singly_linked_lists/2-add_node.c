749b50b6a67ceb521b2ee0ee8555f07537172e7c
Amas5amaso <ansamatemeso@gmail.com>
2023-10-19 07:45:49 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	newnode->len = count;
-	nwenode->next = *head;
+	newnode->next = *head;
 	*head = newnode;
 
 	return (*head);

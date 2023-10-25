8fd5b193210e5846befdec3db12df1c066e6fd44
Amas5amaso <ansamatemeso@gmail.com>
2023-10-25 04:13:39 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,4 +10,14 @@
 
 size_t listint_len(const listint_t *h)
 {
+	size_t element;
+
+	if (h == NULL)
+		return (0);
+	for (element = 0; h != NULL; element++)
+	{
+		h = h->next;
+	}
+	return (element);
+}
 

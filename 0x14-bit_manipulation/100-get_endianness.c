6a627395be66159ba46f48d038be93dcafd53a3b
Amas5amaso <ansamatemeso@gmail.com>
2023-10-26 17:26:16 -0700

    up to date

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,9 +2,8 @@
 
 /**
  * get_endianness -  function that checks the endianness.
- * @end: check the endianness
  *
- * Return: 0 if bigger
+ * Return: 0 if big endian, 1 if small
  */
 
 int get_endianness(void)

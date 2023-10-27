cf0ea0383d25fe1f62777a670083abeae29acea8
Amas5amaso <ansamatemeso@gmail.com>
2023-10-27 15:31:17 -0700

    Useful update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * _pow - function that calculates base ^ power
- * @base: base of the exponent
- * @power: power of the exponent
- *
- * Return: value of the base ^ power
- */
-
-unsigned long int _power(unsigned int base, unsigned int power)
-{
-	unsigned long int y;
-	unsigned long int x;
-
-	y = 1;
-	for (x = 1; x <= power; x++)
-		y *= base;
-	return (y);
-}
-
-/**
  * print_binary - function that prints the binary
  * representation of a number
  * @n: number to print
@@ -30,13 +11,14 @@ unsigned long int _power(unsigned int base, unsigned int power)
 void print_binary(unsigned long int n)
 {
 	int bit = sizeof(n) * 8;
-	print = 0;
 
-	while (bite)
+	int print = 0;
+
+	while (bit)
 	{
 		if (n & 1l << --bit)
 		{
-			-putchar('1');
+			_putchar('1');
 			print++;
 		}
 		else if (print)

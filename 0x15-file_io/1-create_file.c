4388f3d306cf7fb02203b7942fd77ad0a32512d0
Amas5amaso <ansamatemeso@gmail.com>
2023-11-02 10:13:54 -0700

    up to date

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,12 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (rwr == -1)
 		return (-1);
-	close(fd);
 
+	close(fd);
 	return (1);
 }

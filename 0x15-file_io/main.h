d3467e352a5bcc596d3b1e5756b2ce2604923de5
Amas5amaso <ansamatemeso@gmail.com>
2023-11-03 15:30:23 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
